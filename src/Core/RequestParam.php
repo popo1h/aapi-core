6b7fb39ccf083f09678f961b51b4517e2b175f18
popo1h <popo1h@sina.com>
2017-05-27 16:54:09 +0800

    RequestParam

--- src/Core/RequestParam.php
+++ src/Core/RequestParam.php
@@ -8,4 +8,52 @@ use Popo1h\Support\Traits\StringPack\JsonTrait;
 class RequestParam implements StringPackInterface
 {
     use JsonTrait;
+
+    /**
+     * @var array
+     */
+    private $paramData;
+
+    /**
+     * @param array $paramData
+     */
+    public function __construct($paramData)
+    {
+        $this->paramData = $paramData;
+    }
+
+    protected static function getPackPropertyNames()
+    {
+        return [
+            'paramData',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getParamData()
+    {
+        return $this->paramData;
+    }
+
+    /**
+     * @param string $name
+     * @param mixed $default
+     * @param callable|null $dealFunction
+     * @return mixed
+     */
+    public function getDataByName($name, $default = null, $dealFunction = null)
+    {
+        if (!isset($this->paramData[$name])) {
+            return $default;
+        }
+
+        $getData = $this->paramData[$name];
+        if (is_callable($dealFunction)) {
+            $getData = $dealFunction($getData);
+        }
+
+        return $getData;
+    }
 }
